221879fd35ee891250f978f013d1e895c6777c08
yashita13 <144597921+yashita13@users.noreply.github.com>
2026-04-06 09:59:57 +0530

    Use environment variable for server port

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,7 +1,7 @@
 import "dotenv/config";
 import app from "./app";
 
-const PORT = 5000;
+const PORT=process.env.PORT||5000;
 
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
